f9d797a8a635fcfb594d5da925fc304c193c7828
Fathia Mohammed <mohammedfo5429@gmail.com>
2019-04-24 17:48:12 -0400

    got the icons and css

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,1 +1,72 @@
-import react from 'react';
+import './SeasonDisplay.css'
+import React from 'react';
+// I am going to define a new funtion above my function season display whose sole purpose is to determine what the season is
+//  we could definetly put this in the Season display body 
+// but extracting as much logic out of function componet as possible is good 
+
+const seasonConfig = {
+summer:{
+    text: "Let's hit the beach",
+    iconName: 'sun'
+},
+winter: {
+    text:"Brr, it's cold!",
+    iconName:'snowflake'
+}
+};
+
+
+const getSeason = (lat, month) => {
+// before we start adding any logic into the function lets check to see if we can call this function inside of the SeasonDisplay function
+// now lets create the function
+
+if(month > 2 && month <9) {
+    // we are going to use a javascript ternary expression to determine if we are in the N or S hemesphiere
+ return lat > 0 ? 'summer' : 'winter';
+//  if lat is > 0 then we mmust be in the northen hemisphere
+// if we are in the northern hemisphere I want to return the value "summer" else "winter"
+// this expresion will be evaluated: if it returns true , we are going to return summer else we are going to return winter
+// this is only going to handle the summer months
+}
+else {
+    // if its between those months return the top 
+    // else if the months is <2 && >9 and the lat is > 0 return this
+    // if we are in the northen hemisphere during the winter months that means we are in winter other wise we must be in the southern hemisphere its summer
+    return lat > 0 ? 'winter' : 'summer';
+}
+
+};
+
+const SeasonDisplay = (props) => {
+    // console.log(props.lat);
+    // We got our latitued down into our seasondisplay component
+    // So now the only other thing we need to do, is figure out the currrent month
+    // And then use the month and lat to decide if it is summer or winter
+
+    // Getting the month is straight forward 
+    // You can go on to your console and type in new Date().getMonth()
+    // it will print out the current month you are in integer
+    // this is a zero index date so 10 indicates the 11 month of the year  
+
+
+    // We are going to put together a code snipt to essentially determine the current season as a string 
+    // So I want to end up with a simple string that says summer or winter 
+    // this will allow our get season to get the latituted and the month 
+    const season = getSeason(props.lat, new Date().getMonth());
+    // const text =  season === 'winter' ? 'Brr, it is chilly' : 'Lets hit the beach!' 
+
+    const {text, iconName} = seasonConfig[season]; // it is going to return {text, iconName}
+
+    // console.log(season);
+    return (
+    <div className={`season-display ${season}`}>
+    {/* {season === 'winter' ? 'Brr, it is chilly' : 'Lets hit the beach!' } */}
+    {/* you can add it directly into the div like this under return or use a helper function */}
+            <i className={`iconleft massive ${iconName} icon`}/>
+                <h1>{text}</h1>
+            <i className={`iconright massive ${iconName} icon`}/>
+    </div>
+    );
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay'
 
 
 class App extends React.Component {
@@ -10,8 +10,8 @@ class App extends React.Component {
 // in a JS class the constructor function is the very first function that is going to be called on any time an instance of this class is created
 // in other words any time that we create a new instance of App component and show it on the screen the constrctor function is going to be automatically and instatly called
 // before anything else. And so that makes it a very good location for us to initallize our state  when our component is first created
-constructor(props) {
-    super(props);
+// constructor(props) {
+//     super(props);
 // we are initalizing out state object here
 // this state object will eventually contain some different pieces of information important data that is impoetant 
 // and relavant to our component that we are putting together. 
@@ -24,39 +24,50 @@ constructor(props) {
 
 // So the general idea is that we might make use of this state property inside of our JSX OR Other fucntions inside of our components 
 // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT TO THIS.STATE 
-this.state = {
+// this.state = {
+//     lat: null,
+//     errMessage: ''
+// };
+
+// }
+
+// this is equvilant to having a constructor function with the sole purposes of initiallizing state.
+// I works perfectly without constructor
+state = {
     lat: null,
     errMessage: ''
 };
-// the render method is going to be called all the time not best practice to addd the function in there especially if it takes time to load
-// So since the constructor is the first things to get initalized it will initalize this funtion and we will get the location first as well even though it loads slow,
-// so we will have the location before we get to render(lifecycle)
-    window.navigator.geolocation.getCurrentPosition(
-        // to uppdate our state object we call setState 
-        position => {
-           this.setState({ lat: position.coords.latitude});
-            // What we don't want to do here is when we want to update our state object 
-            // this.state.lat = position.coords.latitude 
-            // NEVER DO THIS!!!! Never do a direct assignment to our state object 
-            // the one single exception to this rule is when we initalize our state inside the constructor function 
-        },
-        // console.log(position), so when we console.loged this we saw that it prented out the position we are in. It was an object with a lot of junk but it also included latitute, we want that latiude, manipulate to get that latitude 
-        err => this.setState({errMessage: err.message })
-   );
+
+componentDidMount(){
+    // the render method is going to be called all the time not best practice to addd the function in there especially if it takes time to load
+    // So since the constructor is the first things to get initalized it will initalize this funtion and we will get the location first as well even though it loads slow,
+    // so we will have the location before we get to render(lifecycle)
+        window.navigator.geolocation.getCurrentPosition(
+            // to uppdate our state object we call setState 
+            position => {
+               this.setState({ lat: position.coords.latitude});
+                // What we don't want to do here is when we want to update our state object 
+                // this.state.lat = position.coords.latitude 
+                // NEVER DO THIS!!!! Never do a direct assignment to our state object 
+                // the one single exception to this rule is when we initalize our state inside the constructor function 
+            },
+            // console.log(position), so when we console.loged this we saw that it prented out the position we are in. It was an object with a lot of junk but it also included latitute, we want that latiude, manipulate to get that latitude 
+            err => this.setState({errMessage: err.message })
+       );
 
 }
-
-
 // React says we have to define render
     render() {
        
-        return (
-            <div>
-                Latitude:{this.state.lat} 
-                <br />
-                ErrorMessage: {this.state.errMessage}
-            </div>
-        );
+        if(this.state.errMessage && !this.state.lat) {
+            return <div>{this.state.errMessage}</div>;
+        } 
+        if(!this.state.errMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+        if(!this.state.errMessage && !this.state.lat) {
+            return <div>Loading!</div>;
+        }
     }
 }
 
